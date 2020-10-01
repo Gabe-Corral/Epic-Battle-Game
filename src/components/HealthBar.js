92cd6b75906e5cfae0057b62f96dc4b06401546f
Gabe-Corral <78gabe.corral@gmail.com>
2020-10-01 15:53:14 -0500

    Added logic for battle component

---
+++ src/components/HealthBar.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import {
+  getHealthBarBackgroundColor,
+  defaultColorPallet
+} from './util/HealthBarUtils';
+
+export default function HealthBar(props, {
+  colors = defaultColorPallet,
+  width = 350,
+  height = 24
+}) {
+
+    const [percentage, setCount] = useState(100)
+
+    useEffect(() => {
+      setCount(props.damage)
+    }, [props.damage])
+
+  return (
+    <div onClick={() => setCount()}
+      style={{
+        borderRadius: '3px',
+        border: '1px solid black',
+        maxWidth: width,
+        height
+      }}
+    >
+      <div
+        style={{
+          width: `${[percentage]}%`,
+          height: '100%',
+          minHeight: height,
+          backgroundColor: getHealthBarBackgroundColor(100, colors)
+        }}
+      >
+        &nbsp;
+      </div>
+    </div>
+  );
+}
